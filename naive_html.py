0fa714f9c6a548aea0ff00f1f72a53a074f0d8e9
ZHU Enwei <spiritas@163.com>
2017-08-12 15:56:09 +0800

    change default user-agent list

--- naive_html.py
+++ naive_html.py
@@ -72,13 +72,21 @@ agent_list = [
     "Mozilla/5.0 (Linux; U; Android 1.6; en-us; SonyEricssonX10i Build/R1AA056) AppleWebKit/528.5  (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1",
 ]
 
+agent_list_on_windows = [
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0", 
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11", 
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16", 
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36", 
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15"
+]
+
 def get_html(url, params=None, timeout=10, agent=None, max_fails=3, res_decoding=True, sleep=0):    
     for n_fail in range(max_fails):
         try:
             if agent is not None:
                 res = requests.get(url, params=params, timeout=timeout, headers={'User-Agent': agent})
             else:
-                res = requests.get(url, params=params, timeout=timeout, headers={'User-Agent': random.choice(agent_list)})
+                res = requests.get(url, params=params, timeout=timeout, headers={'User-Agent': random.choice(agent_list_on_windows)})
         except Exception as e:
             pass
         else:
